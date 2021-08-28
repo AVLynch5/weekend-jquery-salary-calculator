65cb81e99e79ac799a6b87fcd36a1d2bd03552e1
TJ Lynch <anthonyvlynch5@gmail.com>
2021-08-28 13:36:34 -0400

    client.js - created function addToDOM

--- client.js
+++ client.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     $('#submit-button').on('click', function() {
         newEmployeeObject();
         calcMonthlySal();
+        //call addToDOM();
     });
 });
 
@@ -42,6 +43,10 @@ function calcMonthlySal() {
     return monthlyTotal;
 }//end calcMonthlySal
 
+function addToDOM() {
+    //function code here
+}//end addToDOM
+
 
 
 
@@ -54,7 +59,7 @@ function calcMonthlySal() {
  * step3: define funct1 - takes no param. Declare var for each input field. Declare newObject with properties. Store input data in newObject. Push newObject to array. Clear input fields. Option - return newObject
  * step4: define funct2 - takes no param. Declare annualTotal var. For loop to sum annual salaries in array of objects. MAKE SURE SUMMIMNG NOT CONCAT. Declare var monthlyTotal = annualTotal/12. return monthlyTotal
  * step5: define funct3 - appends data to the DOM. Problem - if funct3 takes newObject and monthlyTotal as inputs, it can easily add new row to table and update monthly total span on click, HOWEVER hitting delete button will clear row and leave space empty.
- *      **funct 3 - takes monthlyTotal as input. First, clears #employee-table-body. for loop - for each object in array, appends row with required data to #employee-table-body. Appends monthlyTotal to span element and performs check. If <= 20K, green color class. If >20K, red color class.
+ *      **funct 3 - takes monthlyTotal as input (maybe). First, clears #employee-table-body. for loop - for each object in array, appends row with required data to #employee-table-body. Appends monthlyTotal to span element and performs check. If <= 20K, green color class. If >20K, red color class.
  *      **Color change won't work - will likely need to clear div at beginning of funct3 and append h3 with text, span with class green/red, and monthlyTotal
  * Step6: Row delete buttons
  * Step7: create onclick function for row delete buttons - 3 param
